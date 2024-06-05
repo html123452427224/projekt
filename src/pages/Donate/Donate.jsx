69212ca41351de05b7fc930e77b66cd75199c698
honza <jan.fuka@student.spsmb.cz>
2024-06-05 08:56:33 +0200

    first commit

---
+++ src/pages/Donate/Donate.jsx
@@ -0,0 +1,17 @@
+import "./Donate.css"
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+import { Helmet } from "react-helmet"
+
+
+export default function Donate() {
+    return (
+        <>
+        <Helmet>
+            <title>Articles</title>
+        </Helmet>
+        <Header></Header>
+        <Footer></Footer>
+        </>
+    )
+}
